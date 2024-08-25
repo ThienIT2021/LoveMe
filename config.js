b406db00a9733d49742adcc3e24e2783553bd5a5
ThienIT2021 <75975288+ThienIT2021@users.noreply.github.com>
2024-08-26 00:17:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Huỳnh Như à!',
+    introDesc: `Anh nhờ em một việc được không? Em có thể giữ hộ trái tim anh không? Bởi anh rất vụng về, anh sợ rằng anh sẽ làm mất hoặc tặng nó cho một ai khác.`,
+    btnIntro: 'hihi 🥰',
+    btnIntri: 'Không nha 😙',
+    title: 'Hình như anh thấy giữa chúng ta có một sợi dây thì phải 😙',
+    desc: 'Nó buộc trái tim anh với trái tim em, vì thế mà dù có ở cách xa em anh vẫn luôn cảm thấy thật gần. ',
+    btnYes: 'Dạ, Đúng vậy anh 🥰',
+    btnNo: 'Không, Anh mơ à 😙',
+    question:'Em làm bạn gái của anh được không? 🥰',
+    btnReply: 'Gửi cho anh 🥰',
+    reply: 'Dạ, em đồng ý làm bạn gái của anh :))🥰',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Cảm ơn em vì đã đồng ý làm bạn gái của anh.',
+    btnAccept: 'Okiiiii lun 🥰',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
